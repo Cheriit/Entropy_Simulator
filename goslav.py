c7493ae0787cd43e3076600532a16294dc00c230
Goslawq <goslawek1234@gmail.com>
2019-05-20 11:01:45 +0200

    pojemnik

--- goslav.py
+++ goslav.py
@@ -1,3 +1,5 @@
+import random as r
+
 class Atom:
     def __init__(self, x, y, speed_x, speed_y):
         self.pos = (x, y)
@@ -13,9 +15,36 @@ class Atom:
     def access_atom(self):
         return self.pos[0], self.pos[1], self.speed[0], self.speed[1]
 
+    def move(self, step=0.05):
+        self.pos = (self.pos[0] + self.speed[0]*step, self.pos[1] + self.speed[1]*step)
+
+
+class Pojemnik:
+    def __init__(self, x, y):
+        self.width = x
+        self.height = y
+        self.atoms = []
+        for h in range(self.height):
+            self.atoms.append(Atom(0.5, h + 0.5, r.uniform(-1, 1), r.uniform(-1, 1)))
+
+    def tick(self):
+        for i in range(len(self.atoms)):
+            self.atoms[i].move()
+
+    def __str__(self):
+        return f"Pojemnik po rozmiarach {self.width} x {self.height}, zawierający {len(self.atoms)} cząstek"
+
 
 if __name__ == "__main__":
     atomy = []
     for i in range(3):
         atomy.append(Atom(1, 1, 1, 1))
     print(atomy[1].access_atom())
+    pojemnik = Pojemnik(5, 5)
+    for i in range(5):
+        print(pojemnik.atoms[i].access_atom())
+    for i in range(5):
+        pojemnik.atoms[i].move()
+    for i in range(5):
+        print(pojemnik.atoms[i].access_atom())
+    print(pojemnik)

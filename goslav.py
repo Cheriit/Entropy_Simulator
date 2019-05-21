8a88a8c9987a4fe9f6688c26cdd9d424f6a8ea86
Jakub Goslawski <goslawek1234@gmail.com>
2019-05-21 15:05:38 +0200

    dodanie pliku konfiguracyjnego, użycie pliku konfiguracyjnego

--- goslav.py
+++ goslav.py
@@ -1,11 +1,16 @@
 import random as r
+import configparser
+
 
 class Atom:
     def __init__(self, x, y, speed_x, speed_y):
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.config = config['Atom']
         self.pos = (x, y)
         self.speed = (speed_x, speed_y)
-        self.mass = 1
-        self.radius = 0.1
+        self.mass = float(self.config['mass'])
+        self.radius = float(self.config['radius'])
 
     def __str__(self):
         return f"Pozycja: {self.pos}\n" \
@@ -15,21 +20,26 @@ class Atom:
     def access_atom(self):
         return self.pos[0], self.pos[1], self.speed[0], self.speed[1]
 
-    def move(self, step=0.05):
-        self.pos = (self.pos[0] + self.speed[0]*step, self.pos[1] + self.speed[1]*step)
+    def move(self, tickrate=20):
+        self.pos = (self.pos[0] + self.speed[0]*(1/int(tickrate)), self.pos[1] + self.speed[1]*(1/int(tickrate)))
 
 
 class Pojemnik:
     def __init__(self, x, y):
-        self.width = x
-        self.height = y
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.config = config['Pojemnik']
+        self.width = int(self.config['width'])
+        self.height = int(self.config['height'])
+        self.tickrate = int(self.config['tickrate'])
+
         self.atoms = []
         for h in range(self.height):
             self.atoms.append(Atom(0.5, h + 0.5, r.uniform(-1, 1), r.uniform(-1, 1)))
 
     def tick(self):
         for i in range(len(self.atoms)):
-            self.atoms[i].move()
+            self.atoms[i].move(self.tickrate)
 
     def __str__(self):
         return f"Pojemnik po rozmiarach {self.width} x {self.height}, zawierający {len(self.atoms)} cząstek"

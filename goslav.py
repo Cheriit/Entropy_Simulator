7413bbf8ca1029c55b65bcc59627d99d9c7ffb09
Jakub Goslawski <goslawek1234@gmail.com>
2019-05-21 19:26:29 +0200

    generowanie losowej pierwszej kolumny

--- goslav.py
+++ goslav.py
@@ -3,7 +3,7 @@ import configparser
 
 
 class Atom:
-    def __init__(self, x, y, speed_x, speed_y):
+    def __init__(self, x, y, speed_x, speed_y, name):
         config = configparser.ConfigParser()
         config.read('config.ini')
         self.config = config['Atom']
@@ -11,9 +11,11 @@ class Atom:
         self.speed = (speed_x, speed_y)
         self.mass = float(self.config['mass'])
         self.radius = float(self.config['radius'])
+        self.name = name
 
     def __str__(self):
-        return f"Pozycja: {self.pos}\n" \
+        return f"Atom \"{self.name}\"\n" \
+            f"Pozycja: {self.pos}\n" \
             f"Prędkość: {self.speed}\n" \
             f"Stałe: masa = {self.mass}, promień = {self.radius}"
 
@@ -26,6 +28,11 @@ class Atom:
 
 class Pojemnik:
     def __init__(self):
+        """
+        Inicjuje pojemnik, zawierający atomy. Właściwoći definiuje config
+        Na ten moment liczba atomów jest związana z wysokością tablicy i jest jer równa,
+         a każdy atom pojawia się na środku komórki
+        """
         config = configparser.ConfigParser()
         config.read('config.ini')
         self.config = config['Pojemnik']
@@ -35,23 +42,38 @@ class Pojemnik:
         self.number_of_atoms = int(self.config['number_of_atoms'])
 
         self.atoms = []
-        for h in range(self.height):
-            self.atoms.append(Atom(0.5, h + 0.5, r.uniform(-1, 1), r.uniform(-1, 1)))
+        for i in range(self.number_of_atoms):
+            self.atoms.append(Atom(r.uniform(0, 1), r.uniform(0, self.height),
+                                   r.uniform(int(self.config['min_speed']), int(self.config['max_speed'])),
+                                   r.uniform(int(self.config['min_speed']), int(self.config['max_speed'])), i))
 
     def tick(self):
         for i in range(len(self.atoms)):
             self.atoms[i].move(self.tickrate)
 
+    def atoms_pos(self):
+        output = []
+        for i in range(self.number_of_atoms):
+            output.append(self.atoms[i].pos)
+        return output
+
+    def atoms_speed(self):
+        output = []
+        for i in range(self.number_of_atoms):
+            output.append(self.atoms[i].speed)
+        return output
+
     def __str__(self):
-        return f"Pojemnik po rozmiarach {self.width} x {self.height}, zawierający {len(self.atoms)} cząstek"
+        return f"Pojemnik po rozmiarach {self.width} x {self.height}, zawierający {len(self.atoms)} atomów"
 
 
 if __name__ == "__main__":
     atomy = []
     for i in range(3):
-        atomy.append(Atom(1, 1, 1, 1))
+        atomy.append(Atom(1, 1, 1, 1, 1))
     print(atomy[1].access_atom())
     pojemnik = Pojemnik()
+    print(atomy[0])
     for i in range(5):
         print(pojemnik.atoms[i].access_atom())
     for i in range(5):
